26f38ac6755b418f191e6798ea84d93cd05fee96
CodyDaniel1 <84569646+CodyDaniel1@users.noreply.github.com>
2023-06-06 18:04:29 +1200

    Create fxmanifest.lua
    : )

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'adamant'
+author 'CodyDaniel1'
+description 'View Who Is Talking, Optimized Resource 😀'
+game { 'gta5' }
+version '1.0.0'
+
+client_scripts {
+    "**/cl_talking.lua",
+}
