623fd0c6f76aceb9d22a98d8f381e48a78f6399a
CodyDaniel1 <84569646+CodyDaniel1@users.noreply.github.com>
2024-03-20 11:01:22 +1300

    More Optimization

--- cl_talking.lua
+++ cl_talking.lua
@@ -1,30 +1,26 @@
-local startingHeight = 0.01 
-local titleColor = '~b~' 
-local outerIdColor = '~b~' 
-local innerIdColor = '~b~' 
-local defaultNameColor = '~b~'
-local isTalking = {} 
+local startingHeight = 0.01 -- How far it is from top of screen. Limit is 0.10
+local titleColor = '~b~' -- Currently Talking Text
+local outerIdColor = '~w~' -- [] Brackets 
+local innerIdColor = '~w~' -- PlayerID
+local defaultNameColor = '~w~' -- PlayerName
 
 Citizen.CreateThread(function()
     while true do
-        local t = 0
-        local activePlayers = GetActivePlayers()
-        for _, playerId in ipairs(activePlayers) do
-            isTalking[playerId] = NetworkIsPlayerTalking(playerId)
-        end
-        for _, playerId in ipairs(activePlayers) do
-            local playerName = GetPlayerName(playerId)
-            if playerName and isTalking[playerId] then
-                local name = outerIdColor .. '[' .. innerIdColor .. GetPlayerServerId(playerId) .. outerIdColor .. '] ' .. defaultNameColor .. playerName
-                t = t + 1
-                if t == 1 then
-                    drawTxt2(0.5, startingHeight, 1.1, 1.0, 0.40, titleColor .. "Currently Talking", 255, 255, 255, 255)
-                end
-                drawTxt2(0.5, startingHeight + (t * 0.030), 1.0704, 1.0, 0.40, name, 255, 255, 255, 255)
+        Citizen.Wait(0)
+        local talkingPlayers = {} -- Store talking players temporarily to avoid multiple loops
+        for i = 0, 255 do
+            if NetworkIsPlayerActive(i) and NetworkIsPlayerTalking(i) then
+                local name = outerIdColor .. '[' .. innerIdColor .. GetPlayerServerId(i) .. outerIdColor .. '] ' .. defaultNameColor .. GetPlayerName(i)
+                table.insert(talkingPlayers, name)
             end
         end
 
-        Citizen.Wait(0)
+        if #talkingPlayers > 0 then
+            drawTxt2(0.5, startingHeight, 1.1, 1.0, 0.40, titleColor .. "Currently Talking:", 255, 255, 255, 255)
+            for index, playerName in ipairs(talkingPlayers) do
+                drawTxt2(0.5, startingHeight + (index * 0.030), 1.0704 + ((string.len(playerName) - 9) * 0.0001), 1.0, 0.40, playerName, 255, 255, 255, 255)
+            end
+        end
     end
 end)
 
