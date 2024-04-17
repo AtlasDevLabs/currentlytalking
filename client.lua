96d15525576151627adc303b97a123cd1a470639
danboi01 <84569646+danboi01@users.noreply.github.com>
2024-04-18 09:51:34 +1200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,32 +1,32 @@
-local startingHeight = 0.01 -- How far it is from top of screen. Limit is 0.10
-local titleColor = '~b~' -- Currently Talking Text
-local outerIdColor = '~w~' -- [] Brackets 
-local innerIdColor = '~w~' -- PlayerID
-local defaultNameColor = '~w~' -- PlayerName
+startingHeight = 0.01 -- How far it is from top of screen. Limit is 0.10
+titleColor = '~b~' -- Currently Talking Text
+outerIdColor = '~w~' -- [] Brackets 
+innerIdColor = '~w~' -- PlayerID
+defaultNameColor = '~w~' -- PlayerName
 
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
-        local talkingPlayers = {} -- Store talking players temporarily to avoid multiple loops
+        local talkingPlayers = {} 
         for i = 0, 255 do
             if NetworkIsPlayerActive(i) and NetworkIsPlayerTalking(i) then
                 local name = outerIdColor .. '[' .. innerIdColor .. GetPlayerServerId(i) .. outerIdColor .. '] ' .. defaultNameColor .. GetPlayerName(i)
                 table.insert(talkingPlayers, name)
             end
         end
-
         if #talkingPlayers > 0 then
-            drawTxt2(0.5, startingHeight, 1.1, 1.0, 0.40, titleColor .. "Currently Talking:", 255, 255, 255, 255)
+            drawTxt(0.5, startingHeight, 1.1, 1.0, 0.40, titleColor .. "Currently Talking:", 255, 255, 255, 255)
             for index, playerName in ipairs(talkingPlayers) do
-                drawTxt2(0.5, startingHeight + (index * 0.030), 1.0704 + ((string.len(playerName) - 9) * 0.0001), 1.0, 0.40, playerName, 255, 255, 255, 255)
+                drawTxt(0.5, startingHeight + (index * 0.030), 1.0704 + ((string.len(playerName) - 9) * 0.0001), 1.0, 0.40, playerName, 255, 255, 255, 255)
             end
         end
     end
 end)
 
-function drawTxt2(x, y, width, height, scale, text, r, g, b, a)
-    SetTextFont(0)
+function drawTxt(x, y, width, height, scale, text, r, g, b, a)
+    SetTextFont(6)
     SetTextScale(scale, scale)
+    SetTextCentre(true)
     SetTextOutline()
     SetTextJustification(0)
     SetTextEntry("STRING")
