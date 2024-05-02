0ea4c899ff85d7cc7cecda2a33b8ea344b9d1b10
Atlas <84569646+AtlasDevLabs@users.noreply.github.com>
2024-05-02 17:45:07 +1200

    Update client.lua
    V2

--- client.lua
+++ client.lua
@@ -1,27 +1,8 @@
-startingHeight = 0.01 -- How far it is from top of screen. Limit is 0.10
-titleColor = '~b~' -- Currently Talking Text
-outerIdColor = '~w~' -- [] Brackets 
-innerIdColor = '~w~' -- PlayerID
-defaultNameColor = '~w~' -- PlayerName
-
-Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(0)
-        local talkingPlayers = {} 
-        for i = 0, 255 do
-            if NetworkIsPlayerActive(i) and NetworkIsPlayerTalking(i) then
-                local name = outerIdColor .. '[' .. innerIdColor .. GetPlayerServerId(i) .. outerIdColor .. '] ' .. defaultNameColor .. GetPlayerName(i)
-                table.insert(talkingPlayers, name)
-            end
-        end
-        if #talkingPlayers > 0 then
-            drawTxt(0.5, startingHeight, 1.1, 1.0, 0.40, titleColor .. "Currently Talking:", 255, 255, 255, 255)
-            for index, playerName in ipairs(talkingPlayers) do
-                drawTxt(0.5, startingHeight + (index * 0.030), 1.0704 + ((string.len(playerName) - 9) * 0.0001), 1.0, 0.40, playerName, 255, 255, 255, 255)
-            end
-        end
-    end
-end)
+local startingHeight = 0.01 -- How far it is from top of screen. Limit is 0.10
+local titleColor = '~b~' -- Currently Talking Text
+local outerIdColor = '~w~' -- [] Brackets 
+local innerIdColor = '~w~' -- PlayerID
+local defaultNameColor = '~w~' -- PlayerName
 
 function drawTxt(x, y, width, height, scale, text, r, g, b, a)
     SetTextFont(6)
@@ -33,4 +14,18 @@ function drawTxt(x, y, width, height, scale, text, r, g, b, a)
     AddTextComponentString(text)
     DrawText(x, y)
 end
- 
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+        for _, player in ipairs(GetActivePlayers()) do
+            local talking = NetworkIsPlayerTalking(player)
+            local playerId = GetPlayerServerId(player)
+            local playerName = GetPlayerName(player)
+            local name = outerIdColor .. '[' .. innerIdColor .. playerId .. outerIdColor .. '] ' .. defaultNameColor .. playerName
+            if talking then
+                drawTxt(0.5, startingHeight, 1.0, 1.0, 0.35, titleColor .. "Currently Talking: \n" .. name, 255, 255, 255, 255)
+            end
+        end
+    end
+end)
